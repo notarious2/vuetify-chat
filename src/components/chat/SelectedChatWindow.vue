36d34b4f1ed614176d9f23dc9f19800b67b59948
notarious <notarious2@gmail.com>
2024-01-17 16:31:04 +0500

    Add component to send attachments Part I

--- src/components/chat/SelectedChatWindow.vue
+++ src/components/chat/SelectedChatWindow.vue
@@ -8,26 +8,50 @@
         @select="onSelectEmoji" />
     </div>
 
+    <v-dialog width="500" v-model="showAttachmentPreview" :close-on-content-click="false">
+      <v-card class="rounded-lg">
+        <v-icon icon="mdi-close" class="ml-auto mr-2 mt-2 mb-n3" color="black" @click="closePreview" />
+        <v-card-title>
+          Attachment Preview
+        </v-card-title>
+
+        <v-img v-if="isPreviewImage" :src="previewImageUrl" height="500" />
+        <p v-else class="text-center">
+          <v-icon icon="mdi-file-pdf-box my-auto" size="x-large" color="teal"></v-icon>
+        </p>
+        <v-card-text>
+          {{ previewFileName }}
+        </v-card-text>
+        <v-card-text>
+          {{ previewFileSize }} Mb
+        </v-card-text>
+        <v-btn color="primary" class="my-5 mx-2">Send</v-btn>
+      </v-card>
+
+    </v-dialog>
     <!-- SEND BUTTON COMPONENT START -->
     <v-card class="rounded-0 rounded-be-lg">
       <v-row align="center" justify="center" no-gutters>
+        <p class="ml-2 mr-n2">
+          <v-file-input class="file-input text-icons font-weight-bold" @update:model-value="handleFileUpload" />
+        </p>
 
-        <v-icon class="ml-2" :class="compactView ? 'mr-2' : ''" size="x-large" color="icons"
-          style="transform: rotate(10deg)">mdi-paperclip</v-icon>
-        <v-icon v-if="!compactView" class="ml-2 mr-2" size="x-large" color="icons"
-          :class="{ activeEmoji: showEmoji }" @click="toggleEmoji">mdi-emoticon-happy-outline</v-icon>
+        <v-icon v-if="!compactView" class="ml-2 mr-2" size="x-large" color="icons" :class="{ activeEmoji: showEmoji }"
+          @click="toggleEmoji">mdi-emoticon-happy-outline</v-icon>
         <!-- @keydown.enter.exact.prevent -> Prevents next line on clicking ENTER -->
         <!-- We should be able to add a new line by pressing SHIFT+ENTER -->
-        <v-textarea ref="textInput" @keydown.enter="sendMessage" hide-details label="Type your text" rows="1" v-model="messageToSend" auto-grow
-          variant="solo"
-          @input="websocketStore.handleUserTyping" :readonly="inputLocked || loadingMessages"></v-textarea>
+        <v-textarea ref="textInput" @keydown.enter="sendMessage" hide-details label="Type your text" rows="1"
+          v-model="messageToSend" auto-grow variant="solo" @input="websocketStore.handleUserTyping"
+          :readonly="inputLocked || loadingMessages"></v-textarea>
 
-        <v-btn @click="sendMessage" icon="mdi-send" variant="plain" :color="messageToSend === '' ? 'blue-grey-lighten-2' : 'send'"
-          size="x-large" class="ml-0" style="font-size: 30px; transform: rotate(-5deg);">
+        <v-btn @click="sendMessage" icon="mdi-send" variant="plain"
+          :color="messageToSend === '' ? 'blue-grey-lighten-2' : 'send'" size="x-large" class="ml-0"
+          style="font-size: 30px; transform: rotate(-5deg);">
         </v-btn>
       </v-row>
 
-      <v-row v-show="friendTyping" class="mb-1 mt-0 ml-5 text-teal-darken-3"> <strong>{{currentFriendFirstName}}</strong>&nbsp;is typing
+      <v-row v-show="friendTyping" class="mb-1 mt-0 ml-5 text-teal-darken-3">
+        <strong>{{ currentFriendFirstName }}</strong>&nbsp;is typing
         <ThreeDots class="ml-n3" />
       </v-row>
       <v-row v-show="!friendTyping" class="mb-1 mt-0 ml-5" style="user-select: none;">&nbsp;</v-row>
@@ -63,6 +87,7 @@ const mainStore = useMainStore();
 const messageStore = useMessageStore();
 const userStore = useUserStore();
 
+
 const { currentChatGUID, inputLocked, friendTyping, currentFriendFirstName } = storeToRefs(chatStore);
 const { socket } = storeToRefs(websocketStore);
 const { compactView } = storeToRefs(mainStore);
@@ -74,6 +99,12 @@ const { currentChatMessages, loadingMessages } = storeToRefs(messageStore);
 const messageToSend = ref("");
 const textInput = ref(null);
 
+const showAttachmentPreview = ref(false);
+const previewFileName = ref("");
+const previewFileSize = ref(null);
+const previewImageUrl = ref("");
+const isPreviewImage = ref(false);
+
 const showEmoji = ref(false);
 
 const toggleEmoji = () => {
@@ -81,6 +112,40 @@ const toggleEmoji = () => {
     textInput.value.focus()
   }
   showEmoji.value = !showEmoji.value
+}
+
+
+const closePreview = () => {
+  isPreviewImage.value = false;
+  showAttachmentPreview.value = false;
+}
+
+const createPreviewData = (file) => {
+  file = file[0]
+  console.log("FILE", file);
+  if (file.type === "image/jpeg") {
+
+    const reader = new FileReader();
+
+    reader.onload = e => {
+      previewImageUrl.value = e.target.result;
+    };
+    isPreviewImage.value = true;
+    reader.readAsDataURL(file);
+  }
+  previewFileSize.value = file.size / 1000000
+  previewFileName.value = file.name
+
+}
+
+
+const handleFileUpload = (file) => {
+  if (!file) {
+    return;
+  }
+  createPreviewData(file);
+  console.log("File Uploaded", file);
+  showAttachmentPreview.value = true;
 }
 
 const onSelectEmoji = (emoji) => {
@@ -111,21 +176,21 @@ const sendMessage = async () => {
     showEmoji.value = false;
     // append messages without confirmation from websocket
     currentChatMessages.value.unshift(
-        {
-          user_guid: currentUser.value.userGUID,
-          chat_guid: currentChatGUID,
-          content: messageToSend.value,
-          created_at: new Date(),
-          is_read: false,
-          is_sending: true,
-        }
-        );
-      // Clear the input field
-      messageToSend.value = "";
-      // scroll to bottom when own new message is appended (after DOM update)
-      nextTick(() => {
-        chatStore.scrollToBottom("smooth");
-      })
+      {
+        user_guid: currentUser.value.userGUID,
+        chat_guid: currentChatGUID,
+        content: messageToSend.value,
+        created_at: new Date(),
+        is_read: false,
+        is_sending: true,
+      }
+    );
+    // Clear the input field
+    messageToSend.value = "";
+    // scroll to bottom when own new message is appended (after DOM update)
+    nextTick(() => {
+      chatStore.scrollToBottom("smooth");
+    })
 
 
 
@@ -137,5 +202,10 @@ const sendMessage = async () => {
 <style scoped>
 .activeEmoji {
   color: teal !important;
+}
+
+.file-input:deep().v-input__control,
+.file-input:deep().v-input__details {
+  display: none;
 }
 </style>
